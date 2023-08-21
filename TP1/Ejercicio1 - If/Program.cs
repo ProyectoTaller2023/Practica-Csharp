dfe0799e4ffae1d0a5a2cf6e0497ab1f0d4e48c1
Paulo Arrejoria <arrejoriapaulo@gmail.com>
2023-08-21 20:26:00 -0300

    Terminado el Ejercicio 1 tanto con switch como con if

---
+++ TP1/Ejercicio1 - If/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1___If
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese un valor por pantalla: ");
+            string numero = Console.ReadLine();
+
+            if (numero == "1") {
+                Console.WriteLine("El valor ingresado fue UNO");
+            }
+            else {
+                if (numero == "2")
+                {
+                    Console.WriteLine("El valor ingresado fue DOS");
+                }
+                else {
+                    if (numero == "3")
+                    {
+                        Console.WriteLine("El valor ingresado fue TRES");
+                    }
+                    else {
+                        if (numero == "4")
+                        {
+                            Console.WriteLine("El valor ingresado fue CUATRO");
+                        }
+                        else
+                        {
+                            if (numero == "5")
+                            {
+                                Console.WriteLine("El valor ingresado fue CINCO");
+                            }
+                            else
+                            {
+                                if (numero == "6")
+                                {
+                                    Console.WriteLine("El valor ingresado fue SEIS");
+                                }
+                                else
+                                {
+                                    if (numero == "7")
+                                    {
+                                        Console.WriteLine("El valor ingresado fue SIETE");
+                                    }
+                                    else
+                                    {
+                                        if (numero == "8")
+                                        {
+                                            Console.WriteLine("El valor ingresado fue OCHO");
+                                        }
+                                        else
+                                        {
+                                            if (numero == "9")
+                                            {
+                                                Console.WriteLine("El valor ingresado fue NUEVE");
+                                            }
+                                            else
+                                            { 
+                                                Console.WriteLine("El valor ingresado fue OTRO"); 
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TP1/Ejercicio1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese un valor por pantalla: ");
+            string numero = Console.ReadLine();
+
+            switch (numero)
+            {
+                case "1":
+                    Console.WriteLine("El valor ingresado es UNO");
+                    break;
+                case "2":
+                    Console.WriteLine("El valor ingresado es DOS");
+                    break;
+                case "3":
+                    Console.WriteLine("El valor ingresado es TRES");
+                    break;
+                case "4":
+                    Console.WriteLine("El valor ingresado es CUATRO");
+                    break;
+                case "5":
+                    Console.WriteLine("El valor ingresado es CINCO");
+                    break;
+                case "6":
+                    Console.WriteLine("El valor ingresado es SEIS");
+                    break;
+                case "7":
+                    Console.WriteLine("El valor ingresado es SIETE");
+                    break;
+                case "8":
+                    Console.WriteLine("El valor ingresado es OCHO");
+                    break;
+                case "9":
+                    Console.WriteLine("El valor ingresado es NUEVE");
+                    break;
+                default:
+                    Console.WriteLine("El valor ingresado fue OTRO");
+                    break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
