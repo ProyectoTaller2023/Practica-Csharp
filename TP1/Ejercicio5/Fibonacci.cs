cc69a8eeb7375ce8be428f63d7ff27f16964a7b4
Joaquin Arlettaz <jjoaquinarlettaz@gmail.com>
2023-08-21 21:56:29 -0300

    feat: agregar ejercicio 5, suma de fibo

---
+++ TP1/Ejercicio5/Fibonacci.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio5
+{
+    internal class Fibonacci
+    {
+        private int _numberOfSums { get; set; }
+        public Fibonacci(int numberOfSums) { 
+            this._numberOfSums = numberOfSums;
+        }
+
+        public int getSummation()
+        { 
+            int[] fiboNumbers = new int[2];
+            fiboNumbers[0] = 0;
+            fiboNumbers[1] = 1;
+            int sumLastTwo = 0;
+
+            for (int i = 1; i < _numberOfSums; i++){
+                sumLastTwo = fiboNumbers[0] + fiboNumbers[1];
+                fiboNumbers[0] = fiboNumbers[1];
+                fiboNumbers[1] = sumLastTwo;
+            }
+
+            return sumLastTwo;
+        }
+    }
+}
---
+++ TP1/Ejercicio5/Program.cs
@@ -0,0 +1,14 @@
+﻿using Ejercicio5;
+
+namespace Ejercicio3;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Ingrese el número de sumas de Fibonacci que desea calcular: ");
+        int numberOfFiboSum = Convert.ToInt32(Console.ReadLine());
+        var fibo = new Fibonacci(numberOfFiboSum);
+        Console.Write($"El numero de sumas fibo hasta {numberOfFiboSum} es {fibo.getSummation()}");
+    }
+}
