ece317e1e95618989984332d96b0e216707d962e
Paulo Arrejoria <arrejoriapaulo@gmail.com>
2023-08-22 16:54:13 -0300

    Terminados el ejercicio 2 del TP1, usando tanto for como while

---
+++ TP1/Ejercicio2 - For/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2___For
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese un valor entero: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("Ingrese otro valor entero: ");
+            int b = int.Parse(Console.ReadLine());
+
+            int limInferior = Math.Min(a, b);
+            int limSuperior = Math.Max(a, b);
+
+            int sumaTotal = 0;
+
+            for (int i = limInferior; i <= limSuperior; i++)
+            {
+                sumaTotal = sumaTotal + i;
+            }
+
+            Console.WriteLine("Valor minimo: " + limInferior);
+            Console.WriteLine("Valor maximo: " + limSuperior);
+            Console.WriteLine("Promedio: " + (limInferior + limSuperior) / 2.0);
+            Console.WriteLine("Suma total: " + sumaTotal);
+            Console.ReadKey();
+        }
+    }
+}
