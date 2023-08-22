7654f23b7cf3d7fe684f88e94629dd9309929ced
Joaquin Arlettaz <jjoaquinarlettaz@gmail.com>
2023-08-21 21:04:14 -0300

    feat: agregar ejercicio 3, aproximacion a PI con Leibniz

---
+++ TP1/Ejercicio2/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Ejercicio3;
+
+class Program
+{
+    private static readonly double pi = Math.PI;
+    static void Main(string[] args)
+    {
+        Console.Write("Introduce el número de iteraciones para aproximar PI: ");
+        int iterations = Convert.ToInt32(Console.Read());
+        double piAproximation = new Leibniz(iterations).getAproximationOfPi();
+        Console.WriteLine();
+        Console.WriteLine($"El valor aproximado de PI es: {piAproximation}");
+        Console.WriteLine("El valor difiere de Math.PI en un " + CalculatePercentDifference(pi, piAproximation) + "%");
+    }
+
+    static double CalculatePercentDifference(double realValue, double approximation)
+    {
+        return Math.Abs((realValue - approximation) / realValue * 100);
+    }
+}
