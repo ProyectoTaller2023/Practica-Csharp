1830e8ffd7273594b48ea267bc3126f1e4eec259
Paulo Arrejoria <arrejoriapaulo@gmail.com>
2023-08-22 17:36:39 -0300

    Ejercicio 4 TP1 listo

---
+++ TP1/Ejercicio4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 1852; i <= 2023; i++)
+            {
+                if(i % 4 == 0)
+                {
+                    Console.WriteLine("El año " + i + " es bisiesto");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
