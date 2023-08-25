736e5559ded338c3edb3d36795f35bde04f42315
Joaquin Arlettaz <jjoaquinarlettaz@gmail.com>
2023-08-25 16:10:27 -0300

    refactor: agregar clase con metodos de extension

--- TP2/Ejercicio1/Program.cs
+++ TP2/Ejercicio1/Program.cs
@@ -1,11 +1,13 @@
-﻿namespace Ejercicio1;
+﻿using MisUtilidades;
+
+namespace Ejercicio1;
 
 class Program
 {
     static void Main(string[] args)
     {
         string[] opciones = { "Operar triangulo", "Operar circulo", "Operar punto" };
-        int eleccion = Program.getEleccionDesdeMenu(opciones);
+        int eleccion = Utilidades.getEleccionDesdeMenu(opciones);
         Program.SwitchOpciones(eleccion);
     }
 
@@ -29,22 +31,22 @@ class Program
     static void ManejarOpcionTriangulo()
     {
         string[] opciones = { "Obtener area", "Obtener perimetro" };
-        int eleccion = Program.getEleccionDesdeMenu(opciones);
+        int eleccion = Utilidades.getEleccionDesdeMenu(opciones);
 
         Console.WriteLine("Ingrese los puntos del triangulo: ");
-        Punto punto1 = LeerPuntoXY();
-        Punto punto2 = LeerPuntoXY();
-        Punto punto3 = LeerPuntoXY();
+        Punto punto1 = Utilidades.LeerPuntoXY();
+        Punto punto2 = Utilidades.LeerPuntoXY();
+        Punto punto3 = Utilidades.LeerPuntoXY();
 
         var triangulo = new Triangulo(punto1, punto2, punto3);
         switch (eleccion)
         {
             case 1:
-                Program.MostrarResultado("El area del triangulo es: " + triangulo.Area());
+                Utilidades.MostrarResultado("El area del triangulo es: " + triangulo.Area());
                 break;
 
             case 2:
-                Program.MostrarResultado("El perimetro del triangulo es: " + triangulo.Perimetro());
+                Utilidades.MostrarResultado("El perimetro del triangulo es: " + triangulo.Perimetro());
                 break;
         }
     }
@@ -52,10 +54,10 @@ class Program
     static void ManejarOpcionCirculo()
     {
         string[] opciones = { "Obtener area", "Obtener perimetro" };
-        int eleccion = Program.getEleccionDesdeMenu(opciones);
+        int eleccion = Utilidades.getEleccionDesdeMenu(opciones);
 
         Console.WriteLine("Ingrese centro del circulo: ");
-        Punto punto = LeerPuntoXY();
+        Punto punto = Utilidades.LeerPuntoXY();
 
         Console.WriteLine("Ingrese radio del circulo: ");
         double radio = Convert.ToDouble(Console.ReadLine());
@@ -65,11 +67,11 @@ class Program
         switch (eleccion)
         {
             case 1:
-                Program.MostrarResultado("El area del circulo es: " + circulo.Area());
+                Utilidades.MostrarResultado("El area del circulo es: " + circulo.Area());
                 break;
 
             case 2:
-                Program.MostrarResultado("El perimetro del circulo es: " + circulo.Perimetro());
+                Utilidades.MostrarResultado("El perimetro del circulo es: " + circulo.Perimetro());
                 break;
         }
     }
@@ -77,64 +79,18 @@ class Program
     static void ManejarOpcionPunto()
     {
         string[] opciones = { "Obtener distancia a otro punto" };
-        int eleccion = Program.getEleccionDesdeMenu(opciones);
+        int eleccion = Utilidades.getEleccionDesdeMenu(opciones);
 
         Console.WriteLine("Ingrese punto 1: ");
-        Punto punto = LeerPuntoXY();
+        Punto punto = Utilidades.LeerPuntoXY();
 
         switch (eleccion)
         {
             case 1:
                 Console.WriteLine("Ingrese punto 2: ");
-                Punto punto2 = LeerPuntoXY();
-                Program.MostrarResultado("La distancia entre puntos es: " + punto.CalcularDistanciaDesde(punto2));
+                Punto punto2 = Utilidades.LeerPuntoXY();
+                Utilidades.MostrarResultado("La distancia entre puntos es: " + punto.CalcularDistanciaDesde(punto2));
                 break;
         }
-    }
-
-    static int getEleccionDesdeMenu(string[] opciones)
-    {
-        int opcionElegida;
-        string eleccion;
-
-        Console.WriteLine("Ingrese una opcion: ");
-        int iteracion = 0;
-
-        foreach (var item in opciones)
-        {
-            iteracion++;
-            Console.WriteLine(item + " - " + iteracion);
-        }
-        Console.Write("Opcion elegida: ");
-
-        do
-        {
-            eleccion = Console.ReadLine();
-
-            if (!int.TryParse(eleccion, out opcionElegida))
-            {
-                Console.WriteLine("Error: Ingrese un número válido.");
-            }
-        } while (!int.TryParse(eleccion, out opcionElegida));
-        Console.WriteLine("");
-
-        return opcionElegida;
-    }
-
-    static Punto LeerPuntoXY()
-    {
-        Console.WriteLine("Ingrese coordenada X: ");
-        int x = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine("Ingrese coordenada Y: ");
-        int y = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine("");
-
-        return new Punto(x, y);
-    }
-
-    static void MostrarResultado(string resultado)
-    {
-        Console.WriteLine(resultado);
-        Console.ReadKey();
     }
 }
---
+++ TP2/Ejercicio1/Utilidades.cs
@@ -0,0 +1,58 @@
+﻿using Ejercicio1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MisUtilidades
+{
+    internal static class Utilidades
+    {
+        public static int getEleccionDesdeMenu(string[] opciones)
+        {
+            int opcionElegida;
+            string eleccion;
+
+            Console.WriteLine("Ingrese una opcion: ");
+            int iteracion = 0;
+
+            foreach (var item in opciones)
+            {
+                iteracion++;
+                Console.WriteLine(item + " - " + iteracion);
+            }
+            Console.Write("Opcion elegida: ");
+
+            do
+            {
+                eleccion = Console.ReadLine();
+
+                if (!int.TryParse(eleccion, out opcionElegida))
+                {
+                    Console.WriteLine("Error: Ingrese un número válido.");
+                }
+            } while (!int.TryParse(eleccion, out opcionElegida));
+            Console.WriteLine("");
+
+            return opcionElegida;
+        }
+
+        internal static void MostrarResultado(string resultado)
+        {
+            Console.WriteLine(resultado);
+            Console.ReadKey();
+        }
+
+        internal static Punto LeerPuntoXY()
+        {
+            Console.WriteLine("Ingrese coordenada X: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingrese coordenada Y: ");
+            int y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("");
+
+            return new Punto(x, y);
+        }
+    }
+}
