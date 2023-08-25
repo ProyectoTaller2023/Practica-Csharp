1f7aff3203b0af3c883a411c9bf23567ae941974
Joaquin Arlettaz <jjoaquinarlettaz@gmail.com>
2023-08-25 17:44:09 -0300

    feat: agregar repositorio bancas

---
+++ TP2/Ejercicio2/RepositorioBanca.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2
+{
+    internal class RepositorioBanca
+    {
+        private List<Banca> bancas;
+
+        public RepositorioBanca()
+        {
+              this.bancas = new List<Banca>();
+        }
+
+        public Banca? Obtener(string pNumero)
+        {
+              return this.bancas.Find(banca => banca.Numero == pNumero);
+        }
+
+        public void Agregar(Banca pBanca)
+        {
+              this.bancas.Add(pBanca);
+        }
+    }
+}
