05455d1996f77454389e10d00107bf2912a1f7df
Joaquin Arlettaz <jjoaquinarlettaz@gmail.com>
2023-09-05 19:15:20 -0300

    feat: agregar entity context para la db

---
+++ TP2/Ejercicio3/EstacionamientoContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio3
+{
+    internal class EstacionamientoContext : DbContext
+    {
+        public DbSet<IngresoVehiculo> Ingresos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer(@"Server=localhost;Database=EstacionamientoDB;Trusted_Connection=True;TrustServerCertificate=True");
+        }
+    }
+}
